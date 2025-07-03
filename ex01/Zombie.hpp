90fc0384dfe0699965a0fbe0d14326a43fb6fec5
João <jocolognesi@gmail.com>
2025-07-03 19:55:00 -0300

    ex01

---
+++ ex01/Zombie.hpp
@@ -0,0 +1,20 @@
+
+#ifndef	ZOMBIE_HPP
+#define	ZOMBIE_HPP
+
+#define RED 	"\033[31m"
+#define GREEN 	"\033[32m"
+#define RESET	"\033[0m"
+
+#include <string>
+#include <iostream>
+
+class Zombie
+{
+	private:
+		std::string name;
+	public:
+		//
+};
+
+#endif
